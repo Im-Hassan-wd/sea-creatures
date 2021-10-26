c664a54ab518a2e97c2e7ae630f4853325e51f8e
taiwohusayn5@gmail.com <alhassansahih@gmail.com>
2021-10-26 23:17:03 +0100

    connecting to database and getting data

--- index.php
+++ index.php
@@ -1,5 +1,24 @@
 <?php
 
+    //connect to database
+    $connect = mysqli_connect('localhost', 'weird', 'test1234', 'sea_creature');
+
+    //check connection
+    if(!$connect){
+        echo 'Connection error:'. mysqli_connect_error();
+    }
+
+    // queries to get all creatures
+    $sql = 'SELECT name, bio, id FROM creatures';
+
+    // gettting query result
+    $data = mysqli_query($connect, $sql);
+
+    // fetch the resulting rows as an array
+    $creature = mysqli_fetch_all($data, MYSQLI_ASSOC);
+
+    print_r($creature);
+
 
 ?>
 
